b2b26650793d3a0124b703592d842ce176faa1c2
Akash Agarwal <akash191095@gmail.com>
2020-05-23 20:25:27 +0530

    add create article form validation

--- src/components/CreateArticle.js
+++ src/components/CreateArticle.js
@@ -1,5 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
+import validator from "validator";
+import urlParser from "js-video-url-parser";
 import { v4 as uuidv4 } from "uuid";
 import { store } from "../contexts/store";
 import { TextField, Button } from "@material-ui/core";
@@ -7,11 +9,11 @@ import { useInput } from "../hooks/useInput";
 import { useHistory } from "react-router-dom";
 
 const Container = styled.section`
+  margin: 1em;
   form {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    alignitems: center;
     margin-top: 2em;
     > * {
       margin-bottom: 2em;
@@ -23,11 +25,26 @@ function CreateArticle() {
   let articleId;
   // const articleId = "2b3d5dc1-8112-4bb7-bf7a-dffe6559461b";
 
-  const { value: title, bind: bindTitle } = useInput("");
-  const { value: videoUrl, bind: bindVideoUrl } = useInput("");
-  const { value: imageUrl, bind: bindImageUrl } = useInput("");
-  const { value: description, bind: bindDescription } = useInput("");
-  const { value: body, bind: bindBody } = useInput("");
+  const { value: title, bind: bindTitle, setError: setErrorTitle } = useInput(
+    ""
+  );
+  const {
+    value: videoUrl,
+    bind: bindVideoUrl,
+    setError: setErrorVideoUrl,
+  } = useInput("");
+  const {
+    value: imageUrl,
+    bind: bindImageUrl,
+    setError: setErrorImageUrl,
+  } = useInput("");
+  const {
+    value: description,
+    bind: bindDescription,
+    setError: setErrorDescription,
+  } = useInput("");
+  const { value: body, bind: bindBody, setError: setErrorBody } = useInput("");
+  const [hasError, setHasError] = useState(false);
   const globalState = useContext(store);
   const {
     state: {
@@ -39,8 +56,58 @@ function CreateArticle() {
 
   let history = useHistory();
 
+  function validateData() {
+    let passedValidation = true;
+
+    // Title
+    if (validator.isEmpty(title)) {
+      setErrorTitle("Title can't be empty");
+      passedValidation = false;
+    } else {
+      setErrorTitle("");
+    }
+
+    // Video Url
+    const videoUrlData = urlParser.parse(videoUrl);
+    if (
+      !validator.isEmpty(videoUrl) &&
+      (!videoUrlData || videoUrlData.provider !== "youtube")
+    ) {
+      setErrorVideoUrl("Please check the youtube video url");
+      passedValidation = false;
+    } else {
+      setErrorVideoUrl("");
+    }
+
+    // Image Url
+    if (!validator.isEmpty(imageUrl) && !validator.isURL(imageUrl)) {
+      setErrorImageUrl("Please check image url");
+    } else {
+      setErrorImageUrl("");
+    }
+
+    // Description
+    if (validator.isEmpty(description)) {
+      setErrorDescription("Description can't be empty");
+      passedValidation = false;
+    } else {
+      setErrorDescription("");
+    }
+
+    // Body
+    if (validator.isEmpty(body)) {
+      setErrorBody("Body can't be empty");
+      passedValidation = false;
+    } else {
+      setErrorBody("");
+    }
+
+    return passedValidation;
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    if (!validateData()) return setHasError(true);
     let newArticles = [];
 
     if (articleId) {
